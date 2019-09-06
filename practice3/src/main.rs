1f05d485ba5aa69752659bc92ff707ae6e57886c
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-09-07 03:13:13 +0900

    added example of nalgebra

--- practice3/src/main.rs
+++ practice3/src/main.rs
@@ -5,4 +5,17 @@ use na::*;
 
 fn main() {
     println!("Try nalgebra");
+
+    let x = Vector3::new(1, 2, 3);
+    let a = Matrix3::new(1, 0, 0,
+                         0, 1, 0,
+                         0, 0, 1);
+
+    println!("{}", x);
+    println!("{}", a);
+
+    let v = a*x;
+    println!("{}", v);
+
+    println!("{}", v.transpose());
 }
