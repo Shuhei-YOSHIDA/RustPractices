e3fe2c41a50a7d528c46b5a95aad3e4e794ea261
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-09-11 23:28:23 +0900

    now worked commit

--- practice1/src/main.rs
+++ practice1/src/main.rs
@@ -8,8 +8,87 @@ fn bar<'a> (x: &'a i32) {
 }
 
 // generics, traits
-fn hoge<T: std::fmt::Display+std::f64::sin>(x: T) {
-    println!("{}", x.sin());
+fn hoge<T: std::fmt::Display>(x: T) {
+    //println!("{}", x.sin());
+    println!("{}", x);
+}
+
+// Struct
+struct Neko {
+    a : String,
+}
+
+struct Foo {
+    x : i32,
+    y : Vec<f64>,
+    z : String,
+    //z : str, // str primitive is difficult for struct
+    //n : Vec<Neko>, // How to use?
+}
+
+impl Foo {
+    fn x(self) -> i32 {
+        self.x
+    }
+
+    fn y(self) -> Vec<f64> {
+        self.y
+    }
+
+    fn z(self) -> String {
+        self.z
+    }
+
+    //fn n(self) -> Vec<Neko> {
+    //    self.n
+    //}
+}
+
+struct FooBuilder {
+    x : i32,
+    y : Vec<f64>,
+    z : String,
+    //z : str, // str primitive is difficult for struct
+    //n : Vec<Neko>,
+}
+
+impl FooBuilder {
+    fn new() -> Self {
+        Self {x : 0,
+              y : vec![],
+              z : "".to_string(),
+              //n : vec![],
+        }
+    }
+
+    fn x(&mut self, x:i32) -> &mut Self {
+        self.x = x;
+        self
+    }
+
+    fn y(&mut self, y:Vec<f64>) -> &mut Self {
+        self.y = y;
+        self
+    }
+
+    fn z(&mut self, z:String) -> &mut Self {
+        self.z = z;
+        self
+    }
+
+//    fn n(&mut self, n:Vec<Neko>) -> &mut Self {
+//        self.n = n;
+//        self
+//    }
+
+    fn spawn(self) -> Foo {
+        Foo {
+            x : self.x,
+            y : self.y,
+            z : self.z,
+            //n : self.n,
+        }
+    }
 }
 
 fn main() {
@@ -30,4 +109,19 @@ fn main() {
     let f : f64 = 10.;
     //hoge(i);
     hoge(f);
+
+    // Struct, Builder pattern
+//    let neko1 = Neko {a: "NEKO1".to_string()};
+//    let neko2 = Neko {a: "NEKO2".to_string()};
+    let foo = FooBuilder::new().x(10)
+                               .y(vec![111., 222., 333.])
+                               .z("FOO".to_string())
+                               //.n(vec![Neko {a : "NEKO1".to_string()},
+                               //        Neko {a : "NEKO2".to_string()},])
+                               .spawn();
+    let x = foo.x();
+    let y = foo.y();
+    let z = foo.z();
+    //let n = foo.n();
+
 }
